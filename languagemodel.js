a8a3f13396a3653ba278c6f7b93ad324236a3d6f
lealbaugh <l.e.albaugh@gmail.com>
2013-12-03 21:55:10 -0500

    Implements counting for the bigrams.

--- languagemodel.js
+++ languagemodel.js
@@ -16,9 +16,6 @@
 
 // and likewise divide
 // 		(can do start on a line-by-line or overall basis)
-
-
-
 // // ----------------------------------
 
 
@@ -37,6 +34,8 @@ function makeArray(lines) {
 
 function makeUnigrams(choppedtext) {
 	var dict = {};
+	var highestscore = 0;
+	// count the occurences of each word
 	for (var i = 0; i<choppedtext.length; i++) {
 		if (choppedtext[i] in dict){
 			dict[choppedtext[i]] += 1;
@@ -45,19 +44,18 @@ function makeUnigrams(choppedtext) {
 			dict[choppedtext[i]] = 1;
 		}
 	}
-
-	var highestscore = 0;
+	// then use that count to calculate the probability of each word
 	for (var word in dict) {
 		var count = dict[word];
 		var unlikeliness = (-1)*(Math.log(count/choppedtext.length));
-		//storing the numbers as "negative probabilities" helps by 1) ensuring no numbers too small for floats, and 2) allowing us to add them instead of multiplying
+		//storing the numbers as "negative logarithmic probabilities" helps by 1) ensuring no numbers too small for floats, and 2) allowing us to add them instead of multiplying
 		dict[word] = unlikeliness;
 		if (unlikeliness > highestscore) {
 			dict["LEAST_COMMON_PROB"] = unlikeliness;
 		}
-		//dict stores the highest unlikeliness, which can be assigned to words that aren't recognized at all
+		// dict also stores the highest unlikeliness, which can be assigned to words that aren't recognized at all
 	}
-
+	// and write it to the external file
 	fs.writeFile(unigramsfile, JSON.stringify(dict, null, 4), function(err) {
 		if(err) {
 			console.log(err);
@@ -68,11 +66,56 @@ function makeUnigrams(choppedtext) {
 	}); 
 }
 
+function makeBigrams(choppedtext) {
+	var dict = {};
+	var prev = "scene";
+	// count the occurences of each word
+
+	for (var i = 1; i<choppedtext.length; i++) {
+		
+		if (!(prev in dict)) {
+			dict[prev] = {};
+		}
+		if (choppedtext[i] in dict[prev]){
+			// console.log("already there:",prev, choppedtext[i]);
+			dict[prev][choppedtext[i]] += 1;
+		}
+		else {
+			// console.log("new:",prev, choppedtext[i]);
+			dict[prev][choppedtext[i]] = 1;
+		}
+		prev = choppedtext[i];
+	}
+
+	// then use that count to calculate the probability of each word
+	// for (var firstword in dict) {
+	// 	var count = dict[word];
+	// 	var unlikeliness = (-1)*(Math.log(count/choppedtext.length));
+	// 	//storing the numbers as "negative logarithmic probabilities" helps by 1) ensuring no numbers too small for floats, and 2) allowing us to add them instead of multiplying
+	// 	dict[word] = unlikeliness;
+	// 	if (unlikeliness > highestscore) {
+	// 		dict["LEAST_COMMON_PROB"] = unlikeliness;
+	// 	}
+	// 	// dict also stores the highest unlikeliness, which can be assigned to words that aren't recognized at all
+	// }
+	// and write it to the external file
+	fs.writeFile(bigramsfile, JSON.stringify(dict, null, 4), function(err) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			console.log("Bigrams saved to "+bigramsfile);
+		}
+	}); 
+}
+
+
+
 var main = function() {
 	fs.readFile(filename,'utf8', function (err, data) {
 		console.log("file read in!");
 		var choppedtext = makeArray(data);
-		makeUnigrams(choppedtext);
+		makeBigrams(choppedtext);
 	});
 }
 
